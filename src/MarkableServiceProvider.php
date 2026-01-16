5a0628e7d35dda7a8ddc780a6b14476c77a9d072
Siarhei Kulich <siarhei.kulich@gmail.com>
2026-01-16 16:19:27 +0100

    Update `MarkableServiceProvider` to publish migrations and config files during console runtime

--- src/MarkableServiceProvider.php
+++ src/MarkableServiceProvider.php
@@ -11,11 +11,32 @@ final class MarkableServiceProvider extends BaseServiceProvider
 {
     public function boot(Router $router): void
     {
-        $this->publishes([__DIR__.'/../config/markable.php' => config_path('markable.php')], ['markable']);
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../database/migrations/create_marks_table.php.stub' => $this->getMigrationFileName(),
+            ], 'markable-migrations');
+
+            $this->publishes([
+                __DIR__.'/../config/markable.php' => config_path('markable.php'),
+            ], 'markable-config');
+        }
+
+        /*Blueprint::macro('markableCounters', function (array $types) {
+            foreach ($types as $type) {
+                $this->integer("{$type}s_count")->default(0);
+            }
+        });*/
     }
 
     public function register(): void
     {
         $this->mergeConfigFrom(__DIR__.'/../config/markable.php', 'markable');
+    }
+
+    protected function getMigrationFileName(): string
+    {
+        $timestamp = now()->addSecond()->format('Y_m_d_His');
+
+        return database_path("migrations/{$timestamp}_create_marks_table.php");
     }
 }
