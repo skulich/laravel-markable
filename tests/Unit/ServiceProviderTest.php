652b8e342dc85a1ee9a6dc141233e817d90c77cd
Siarhei Kulich <siarhei.kulich@gmail.com>
2026-01-16 16:19:55 +0100

    Add unit tests for config merging, config publishing, and migration publishing

--- tests/Unit/ServiceProviderTest.php
+++ tests/Unit/ServiceProviderTest.php
@@ -1,10 +1,43 @@
 <?php
 
-it('publishes config file', function () {
-    $this->artisan('vendor:publish', ['--tag' => 'markable'])
-        ->assertSuccessful();
-
-    expect(config_path('markable.php'))->toBeFile();
-
-    unlink(config_path('markable.php'));
+it('merges config in register method', function () {
+    expect(config('markable'))->toBeArray();
 });
+
+it('publishes config file', function () {
+
+    $configPath = config_path('markable.php');
+
+    if (File::exists($configPath)) {
+        unlink($configPath);
+    }
+
+    $this->artisan('vendor:publish', [
+        '--provider' => 'SKulich\Markable\MarkableServiceProvider',
+        '--tag' => 'markable-config'
+    ])->assertSuccessful();
+
+    expect($configPath)->toBeFile();
+
+    unlink($configPath);
+});
+
+it('publishes migration file', function () {
+
+    foreach (File::glob(database_path('migrations/*_create_marks_table.php')) as $file) {
+        unlink($file);
+    }
+
+    $this->artisan('vendor:publish', [
+        '--provider' => 'SKulich\Markable\MarkableServiceProvider',
+        '--tag' => 'markable-migrations'
+    ])->assertSuccessful();
+
+    $migrations = File::glob(database_path('migrations/*_create_marks_table.php'));
+
+    expect($migrations)->not->toBeEmpty();
+    expect($migrations[0])->toBeFile();
+
+    unlink($migrations[0]);
+});
+
